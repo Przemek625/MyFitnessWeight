bd49ed0a6a63cca0b530a2111260c84158b042f6
Przemek <przemek625@gmail.com>
2016-12-31 03:39:49 +0100

    initial configuration of my project

--- MyFitnessWeight/settings.py
+++ MyFitnessWeight/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fitnesau',
+    'fitness',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyFitnessWeight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'fitness/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- fitness/apps.py
+++ fitness/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class FitnesauConfig(AppConfig):
-    name = 'fitnesau'
+class FitnessConfig(AppConfig):
+    name = 'fitness'
